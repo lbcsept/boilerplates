be18e7d250721e43795c8d3004b18a4b534a5544
lbcsept <lbcsept15@gmail.com>
2026-01-11 12:26:01 +0100

    Refactor API and switch logic for brain control; improved logging and error handling

--- docker-compose/brain/src/api.py
+++ docker-compose/brain/src/api.py
@@ -8,24 +8,18 @@ app = Flask(__name__)
 
 
 @app.route('/on', methods=['POST'])
-def on_test():
+def on():
     # Your Python logic here
     logging.info("Triggering on brain")
-    logging.info(f"BRAIN_MAC:  {os.environ.get('BRAIN_MAC')}")
-    logging.info(f"BRAIN_HOST: {os.environ.get('BRAIN_HOST')}")
-    logging.info(f"BRAIN_MAC: {os.environ.get('BRAIN_MAC')}")
-    logging.info(f"BRAIN_USER: {os.environ.get('BRAIN_USER')}")
-    logging.info(f"BRAIN_PSW: {os.environ.get('BRAIN_PSW')}")
-    logging.info(f"PYTHONPATH: {os.environ.get('PYTHONPATH')}")
-    switch_on()
-    return jsonify({"status": "turning brain on"})
+    logging.debug(f"BRAIN_MAC:  {os.environ.get('BRAIN_MAC')}")
+    logging.debug(f"BRAIN_HOST: {os.environ.get('BRAIN_HOST')}")
+    return jsonify(switch_on())
+    # return jsonify({"status": "turning brain on"})
 
 @app.route('/off', methods=['POST'])
 def off():
-    # Your Python logic here
-    # logging.info("Triggering off brain")
-    switch_off()
-    return jsonify({"status": "switching off brain"})
+    logging.info("Triggering off brain")
+    return jsonify(switch_off())
 
 @app.route('/trigger', methods=['POST'])
 def trigger_code():
