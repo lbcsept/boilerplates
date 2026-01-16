d9ee82d5fe99f9ae96bb7e9961a2ad5c39490068
lbcsept <lbcsept15@gmail.com>
2026-01-16 08:46:22 +0100

    added alexa-endpoint

--- docker-compose/brain/src/api.py
+++ docker-compose/brain/src/api.py
@@ -3,11 +3,15 @@ from .switch import on as switch_on, off as switch_off
 import os
 import logging
 
+log_file = 'brain_app.log'
+
 app = Flask(__name__)
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename=log_file,  # Specify the log file name
+    filemode='a'  # Append mode
 )
 
 @app.route('/on', methods=['POST'])
@@ -28,6 +32,40 @@ def off():
     logging.info(f"{client_ip} is switching brain OFF: {res}")
     return jsonify(res)
 
+@app.route('/alexa-endpoint', methods=['POST'])
+def handle_alexa_request():
+    data = request.json
+    logging.info("Received from Alexa:", data)
+
+    # Extract intent and slots
+    intent = data.get('request', {}).get('intsent', {}).get('name')
+    onoff = data.get('request', {}).get('intent', {}).get('slots', {}).get('onoff', {}).get('value')
+    action = "allumer"
+    if onoff == 'on':
+        res = switch_on()
+    else:
+        action = "éteindre"
+        res = switch_off()
+
+    mess = f"brain va s'{action}"   
+    if res.get("status") != "success":
+        mess = f"une erreur s'est produite lors de la commande pour {action} brain"
+
+    logging.info(res)
+
+    # Your logic here (e.g., toggle a switch)
+    response = {
+        "version": "1.0",
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": mess
+            }
+        }
+    }
+
+    return jsonify(response)
+
 @app.route('/authorize', methods=['POST'])
 def authorize():
     client_ip = request.remote_addr
@@ -43,7 +81,22 @@ def status():
     return jsonify({"status": "success", 
           "message": f"I am fine (status not yet implemented)"})
 
+@app.route('/token', methods=['GET'])
+def token():
+    client_ip = request.remote_addr
+    return jsonify({"status": "success", 
+          "message": f"I am fine (status not yet implemented)"})
 
+@app.route('/logs', methods=['GET'])
+def get_logs():
+    n = request.args.get('n', default=10, type=int)  # Default to 10 lines if 'n' is not provided
+    try:
+        with open(log_file, 'r') as file:
+            lines = file.readlines()
+            last_n_lines = lines[-n:] if len(lines) >= n else lines
+            return jsonify({'logs': last_n_lines})
+    except FileNotFoundError:
+        return jsonify({'error': 'Log file not found'}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
